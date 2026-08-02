b0c78d422914ffa8a0d23ac472fe50599c8d4d03
HelomInerreli <helom.valentim.t0127856@edu.atec.pt>
2026-08-02 16:07:55 +0000

    feat: Update baseURL for API requests to use the correct endpoint

--- frontend/client_app/src/api/http.ts
+++ frontend/client_app/src/api/http.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://sturdy-yodel-r4p9vpqq6qv52xw5p-8000.app.github.dev/api/v1",
+  baseURL: import.meta.env.VITE_API_URL || "https://new-final-project-atec.onrender.com/api/v1",
   // baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api/v1",
 });
 
